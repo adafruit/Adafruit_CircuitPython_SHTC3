b830a148dc0dfb732330dbaaea540aa961a53586
dherrada <dylan.herrada@adafruit.com>
2022-11-18 13:05:29 -0500

    Added commented out board.STEMMA_I2C with explanation

--- examples/shtc3_simpletest.py
+++ examples/shtc3_simpletest.py
@@ -6,6 +6,7 @@ import board
 import adafruit_shtc3
 
 i2c = board.I2C()  # uses board.SCL and board.SDA
+# i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
 sht = adafruit_shtc3.SHTC3(i2c)
 
 while True:
